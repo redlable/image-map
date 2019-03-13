f2e95702098a02f981a49057a2f723cc84408c59
Anton Rylov <anton.rylov@propeople.com.ua>
2019-03-13 11:36:06 +0200

    Image map

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-let anchorHash = window.location.hash;
+"use strict";
+
+var anchorHash = window.location.hash;
 
 // delete hash so the page won't scroll to it
 if (window.location.hash.length) {
   window.location.hash = "";
 }
 
-(function($) {
+(function ($) {
   'use strict';
 
   // Detect IE browser.
-  let ua = window.navigator.userAgent;
-  let msie = ua.indexOf("MSIE ");
+
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
 
   if (msie > 0) {
     $('body').addClass('ie ie' + parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
-  }
-  else if (!!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+  } else if (!!navigator.userAgent.match(/Trident.*rv\:11\./)) {
     $('body').addClass('ie ie11');
   }
 
   // Detect Firefox.
-  if( navigator.userAgent.toLowerCase().indexOf('firefox') > -1 ){
+  if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
     $('body').addClass('firefox');
   }
 
@@ -31,7 +33,7 @@ if (window.location.hash.length) {
 
   // Detect mobile device.
   function isTouchDevice() {
-    let $body = $('body');
+    var $body = $('body');
 
     if ('ontouchstart' in document) {
       $body.addClass('mobile');
@@ -47,33 +49,33 @@ if (window.location.hash.length) {
   isTouchDevice();
 
   // Toggle mobile and desktop behavior
-  let $body = $('body');
+  var $body = $('body');
 
   window.innerWidth < 1024 ? $body.addClass('mobile-behavior') : $body.addClass('desktop-behavior');
 
-  $.resizeAction(function() {
+  $.resizeAction(function () {
     return window.innerWidth < 1024;
-  }, function(isTrue) {
+  }, function (isTrue) {
     isTrue ? $body.removeClass('desktop-behavior').addClass('mobile-behavior') : $body.removeClass('mobile-behavior').addClass('desktop-behavior');
     !isTrue ? $body.removeClass('active-menu') : '';
   });
 
   // Anchors
-  (function() {
-    let $root = $('html, body'),
+  (function () {
+    var $root = $('html, body'),
         $window = $(window),
         $document = $(document),
         $body = $('body');
 
     // to top right away
-    if (window.location.hash) scroll(0,0);
+    if (window.location.hash) scroll(0, 0);
     // void some browsers issue
     // setTimeout(function() { scroll(0,0); }, 1);
 
-    $window.on('load', function() {
-      let adminMenuHeight = parseInt($body.css('padding-top')) || 0,
+    $window.on('load', function () {
+      var adminMenuHeight = parseInt($body.css('padding-top')) || 0,
           headerHeight = $('#header').outerHeight(),
-          fixedHeaderHeight = $window.width() >= 1024 ? (headerHeight - 20) : 0;
+          fixedHeaderHeight = $window.width() >= 1024 ? headerHeight - 20 : 0;
 
       // *only* if we have anchor on the url
       if (anchorHash) {
@@ -87,14 +89,14 @@ if (window.location.hash.length) {
       }
     });
 
-    $('a[href^=\\#]').on('click', function(e) {
+    $('a[href^=\\#]').on('click', function (e) {
       e.preventDefault();
 
-      let $scrollEl = $(this.hash),
+      var $scrollEl = $(this.hash),
           scrollElOffset = Math.round($scrollEl.offset().top),
           adminMenuHeight = parseInt($body.css('padding-top')),
           headerHeight = $('#header').outerHeight(),
-          fixedHeaderHeight = $window.width() >= 1024 ? (headerHeight - 20) : 0,
+          fixedHeaderHeight = $window.width() >= 1024 ? headerHeight - 20 : 0,
           speed = 400;
 
       if (scrollElOffset > $document.scrollTop() + $window.height()) {
@@ -107,50 +109,49 @@ if (window.location.hash.length) {
     });
 
     // Stop scroll animation on user scroll
-    $window.on('touchmove mousewheel', function(){
+    $window.on('touchmove mousewheel', function () {
       $root.stop();
     });
   })();
 
   // Sliders
   (function slider() {
-    let $slider = $('.slider');
+    var $slider = $('.slider');
 
     if ($slider.children().length > 1) {
-      $slider.each(function() {
-        let $slider = $(this),
+      $slider.each(function () {
+        var $slider = $(this),
             options = {
-              autoplay: true,
-              autoplaySpeed: 8000,
-              arrows: false,
-              dots: true,
-              fade: true,
-              speed: 1000
-            };
+          autoplay: true,
+          autoplaySpeed: 8000,
+          arrows: false,
+          dots: true,
+          fade: true,
+          speed: 1000
+        };
 
         $slider.slick(options);
       });
     }
 
     // Guide slider
-    let $guideSlider = $('.guide-slider').find('.step-container'),
+    var $guideSlider = $('.guide-slider').find('.step-container'),
         guideSliderOpt = {
-          arrows: false,
-          infinite: false
-        };
+      arrows: false,
+      infinite: false
+    };
 
     if (window.innerWidth < 768) {
       $guideSlider.slick(guideSliderOpt);
     }
 
-    $.resizeAction(function() {
+    $.resizeAction(function () {
       return window.innerWidth < 768;
-    }, function(isTrue) {
+    }, function (isTrue) {
       if (!isTrue) {
 
         $guideSlider.slick('unslick');
-      }
-      else {
+      } else {
         if (!$guideSlider.is('.slick-initialized')) {
           $guideSlider.slick(guideSliderOpt);
         }
@@ -160,18 +161,17 @@ if (window.location.hash.length) {
 
   // Accordion
   (function accordion() {
-    $('.faq-header').on('click', function() {
-      let $this = $(this);
+    $('.faq-header').on('click', function () {
+      var $this = $(this);
 
-      $this.closest('.faq-item').siblings().removeClass('active')
-        .find('.faq-content').slideUp();
+      $this.closest('.faq-item').siblings().removeClass('active').find('.faq-content').slideUp();
 
-      $this.siblings('.faq-content').slideToggle(400, function() {
-        let self = $(this);
+      $this.siblings('.faq-content').slideToggle(400, function () {
+        var self = $(this);
 
         if (self.is(':visible') && self.find('iframe').length) {
-          self.find('iframe').each(function(i, el) {
-            let iframe = $(el);
+          self.find('iframe').each(function (i, el) {
+            var iframe = $(el);
 
             if (!iframe.is('.refreshed')) {
               iframe.attr('src', iframe.attr('src'));
@@ -182,19 +182,19 @@ if (window.location.hash.length) {
       }).closest('.faq-item').toggleClass('active');
     });
 
-    $('.faq-item').each(function(i, el) {
-      $(el).css('transition-delay', (i * .1) + 's');
+    $('.faq-item').each(function (i, el) {
+      $(el).css('transition-delay', i * .1 + 's');
     });
   })();
 
   // Scroll animations
   (function scrollAnimaiton() {
-    let $sections = $('.content-container').children();
-    let $rowTitle = $('.group-title');
-    let $rowList = $('.step-item');
+    var $sections = $('.content-container').children();
+    var $rowTitle = $('.group-title');
+    var $rowList = $('.step-item');
 
-    $sections.add($rowList).add($rowTitle).each(function(i, el) {
-      let $el = $(el);
+    $sections.add($rowList).add($rowTitle).each(function (i, el) {
+      var $el = $(el);
 
       if ($el.isVisible(true)) {
         $el.addClass('in-view');
@@ -202,53 +202,53 @@ if (window.location.hash.length) {
     });
 
     $(window).on('scroll', function () {
-      let $window = $(window);
-      let sixthWinHeight = $window.height() / 6;
-      let viewTop = $window.scrollTop();
-      let viewBottom = viewTop + $window.height();
+      var $window = $(window);
+      var sixthWinHeight = $window.height() / 6;
+      var viewTop = $window.scrollTop();
+      var viewBottom = viewTop + $window.height();
 
       $sections.add($rowList).add($rowTitle).each(function (i, el) {
-        let $el = $(el);
-        let elemTopPos = $el.offset().top;
+        var $el = $(el);
+        var elemTopPos = $el.offset().top;
 
-        if ($el.isVisible(true) && elemTopPos < (viewBottom - sixthWinHeight)) {
+        if ($el.isVisible(true) && elemTopPos < viewBottom - sixthWinHeight) {
           $el.addClass("in-view");
         }
       });
     });
 
     // Sticky header
-    let $header = $('#header');
+    var $header = $('#header');
 
     if (window.scrollY > 0) {
       $header.addClass('sticky');
     }
 
-    $.scrollAction(function() {
+    $.scrollAction(function () {
       return $(window).scrollTop() > 0;
-    }, function(isTrue) {
+    }, function (isTrue) {
       isTrue ? $header.addClass('sticky') : $header.removeClass('sticky');
     });
   })();
 
   // Mobile menu
   (function mobileMenu() {
-    let $header = $('#header');
-    let $mobMenuBtn = $('.hamburger-btn');
-    let $closeMenuBtn = $('.close-menu');
+    var $header = $('#header');
+    var $mobMenuBtn = $('.hamburger-btn');
+    var $closeMenuBtn = $('.close-menu');
 
-    $mobMenuBtn.on('click', function() {
+    $mobMenuBtn.on('click', function () {
       $header.addClass('active-menu');
     });
 
-    $closeMenuBtn.on('click', function() {
+    $closeMenuBtn.on('click', function () {
       $header.removeClass('active-menu');
     });
   })();
 
   // Toggle status messages
   (function toggleMessage() {
-    $('.message').on('click', function(e) {
+    $('.message').on('click', function (e) {
       if (!$(e.target).is('a')) {
         $(this).toggleClass('hidden-msg');
       }
@@ -256,10 +256,10 @@ if (window.location.hash.length) {
   })();
 
   (function imageMap() {
-    let $imageMap = $('.image-map');
+    var $imageMap = $('.image-map');
 
-    $imageMap.on('click', function(e) {
-      let $this = $(this),
+    $imageMap.on('click', function (e) {
+      var $this = $(this),
           $target = $(e.target),
           $activeGraph = $('.' + $target.attr('alt'));
 
@@ -267,11 +267,11 @@ if (window.location.hash.length) {
       $activeGraph.addClass('active');
     });
 
-    $('.clear').on('click', function() {
+    $('.clear').on('click', function () {
       $('.graphic').removeClass('active');
     });
 
     $imageMap.imageMapResize();
   })();
-
 })(jQuery);
+//# sourceMappingURL=main.js.map
